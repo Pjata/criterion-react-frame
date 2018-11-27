d7178f58266a99147a60389c5a9d7c76b454249e
Pjatacsuk Bence <pjatacsuk@gmail.com>
2018-11-27 12:35:34 +0100

    criterionproviders update

--- src/components/Form/CriterionProviders.js
+++ src/components/Form/CriterionProviders.js
@@ -4,9 +4,9 @@ import MuiPickersUtilsProvider from "material-ui-pickers/utils/MuiPickersUtilsPr
 import { I18nextProvider } from "react-i18next"
 class CriterionProviders extends PureComponent {
   render() {
-    const { i18n, children } = this.props
+    const { i18n, children, language = "kplogKPLOG" } = this.props
     return (
-      <I18nextProvider i18n={i18n} initialLanguage={"kplogKPLOG"}>
+      <I18nextProvider i18n={i18n} initialLanguage={language}>
         <MuiPickersUtilsProvider utils={MomentUtils}>
           {children}
         </MuiPickersUtilsProvider>
